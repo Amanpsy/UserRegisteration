a23396091bce50f4de0b6a0e4044bec0512524ff
Aman <amankr97111@gmail.com>
2022-08-20 06:55:05 +0530

    first commit

---
+++ UserRegisteration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+using System.Data;
+
+namespace UserRegistration
+{
+    class Program
+    {
+    public  static void Main(string[] args)
+        {
+            TestUser.UserNameValidation User = new TestUser.UserNameValidation();
+            User.Validation();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ UserRegisteration/TestUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUser
+{
+   public class UserNameValidation
+    {
+        public void Regex_User(string input, string pattern)
+        {
+
+            if (Regex.IsMatch(input, pattern))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void Validation()
+        {
+            Console.WriteLine("Enter the user name");
+            string input = Console.ReadLine();
+            string pattern = "^[A-Z][a-z]{3}$";
+            Regex_User(input , pattern);
+        }
+    }
+}
